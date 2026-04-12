c4c9ffbad53e561841b305e634616925c03f529a
Lucy Faria <dev@lucyfaria.net>
2026-04-12 11:41:57 -0400

    adjust release script to only generate binaries for sm 1.11

--- p4sstime-scripts/release.sh
+++ p4sstime-scripts/release.sh
@@ -2,23 +2,25 @@
 # CD to the parent directory of this file (root of repository)
 cd "$(dirname "$0")/.."
 
-echo 'Compiling binary...'
-./p4sstime-scripts/compile_sm_12.sh
+# echo 'Compiling binary...'
+# ./p4sstime-scripts/compile_sm_12.sh
 
 echo 'Compiling binary for SourceMod 1.11...'
 ./p4sstime-scripts/compile_sm_11.sh
 
-NAME12=p4sstime_sm_1_12.smx
+# NAME12=p4sstime_sm_1_12.smx
 NAME11=p4sstime.smx
 
-sm12exists=false
+# sm12exists=false
 sm11exists=false
+
 # If existing, move these plugins out to preserve them
-if [ -f "plugins/$NAME12" ]; then
-  echo "$NAME12 exists"
-  sm12exists=true
-  mv "plugins/$NAME12" "plugins/$NAME12.old"
-fi
+
+# if [ -f "plugins/$NAME12" ]; then
+#   echo "$NAME12 exists"
+#   sm12exists=true
+#   mv "plugins/$NAME12" "plugins/$NAME12.old"
+# fi
 
 if [ -f "plugins/$NAME11" ]; then
   echo "$NAME11 exists"
@@ -28,7 +30,7 @@ fi
 
 # Move compiled binaries to plugins folder for archive
 mkdir -p plugins
-mv "compiled/$NAME12" plugins
+# mv "compiled/$NAME12" plugins
 mv "compiled/$NAME11" plugins
 
 rm -f p4sstime.tar.xz
@@ -50,22 +52,22 @@ zip p4sstime.zip \
   "plugins/$NAME11" \
   gamedata/p4sstime.txt
 
-tar -cJf p4sstime_sm_1_12.tar.xz \
-  "plugins/$NAME12" \
-  gamedata/p4sstime.txt
+# tar -cJf p4sstime_sm_1_12.tar.xz \
+  # "plugins/$NAME12" \
+  # gamedata/p4sstime.txt
 
-zip p4sstime_sm_1_12.zip \
-  "plugins/$NAME12" \
-  gamedata/p4sstime.txt
+# zip p4sstime_sm_1_12.zip \
+  # "plugins/$NAME12" \
+  # gamedata/p4sstime.txt
 
 # Move back plugins to compiled folder
-mv "plugins/$NAME12" compiled
+# mv "plugins/$NAME12" compiled
 mv "plugins/$NAME11" compiled
 
 if [ "$sm11exists" = true ] ; then
   mv "plugins/$NAME11.old" "plugins/$NAME11"
 fi
-if [ "$sm12exists" = true ] ; then
-  mv "plugins/$NAME12.old" "plugins/$NAME12"
-fi
+# if [ "$sm12exists" = true ] ; then
+  # mv "plugins/$NAME12.old" "plugins/$NAME12"
+# fi
 
